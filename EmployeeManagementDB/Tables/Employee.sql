957e60f4e1c1db89ebed378d3bad3c89b1667697
DerekSirois <dereksirois@hotmail.ca>
2023-11-21 20:49:36 -0500

    project start

---
+++ EmployeeManagementDB/Tables/Employee.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [CreatedAt] DATETIME NOT NULL DEFAULT sysdatetime(), 
+    [UpdatedAt] DATETIME NOT NULL DEFAULT sysdatetime(), 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(100) NOT NULL, 
+    [PhoneNumber] NVARCHAR(10) NOT NULL, 
+    [EmployeeNumber] NVARCHAR(50) NOT NULL, 
+    [BirthDay] DATE NOT NULL, 
+    [StartDate] DATE NOT NULL, 
+    [Active] BIT NOT NULL DEFAULT 1, 
+    [SIN] VARBINARY(160) NULL, 
+    [DepartmentId] UNIQUEIDENTIFIER NOT NULL, 
+    CONSTRAINT [FK_Employee_ToDepartment] FOREIGN KEY (DepartmentId) REFERENCES [Department]([Id])
+)
---
+++ EmployeeManagementDB/scripts/Script.PostDeploy.sql
@@ -0,0 +1,29 @@
+﻿/*
+Modèle de script de post-déploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF (NOT EXISTS(SELECT * FROM [dbo].[Department] WHERE Name = 'IT'))
+BEGIN
+    INSERT INTO [dbo].[Department](Name)
+    VALUES('IT')
+END
+
+IF (NOT EXISTS(SELECT * FROM [dbo].[Department] WHERE Name = 'HR'))
+BEGIN
+    INSERT INTO [dbo].[Department](Name)
+    VALUES('HR')
+END
+
+IF (NOT EXISTS(SELECT * FROM [dbo].[Department] WHERE Name = 'Engineering'))
+BEGIN
+    INSERT INTO [dbo].[Department](Name)
+    VALUES('Engineering')
+END
